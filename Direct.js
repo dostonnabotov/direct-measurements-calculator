c3b28ea69996c091e20d3bc3a94e1fd7c3484b02
Doston Nabotov <dostonnabotov@gmail.com>
2022-10-29 18:55:09 +0300

    calculations, guide update

---
+++ Direct.js
@@ -0,0 +1,96 @@
+export default class DirectMeasurement {
+  /**
+   * Calculate the direct measurement
+   * @class
+   * @param {array} nums - the array of measurements
+   * @param {number} smallestScale - the smallest scale value
+   * @param {number} coefficientN - the coefficient number, corresponding to the confidence limit, for the number of measurements made
+   */
+  constructor(nums, smallestScale, coefficientN, type) {
+    this.nums = Array.isArray(nums) ? nums : [];
+    this.smallestScale = isNaN(smallestScale) ? 0 : smallestScale;
+    this.coefficientN = isNaN(coefficientN) ? 0 : coefficientN;
+    this.type = type;
+    this.coefficientInf = 1.96;
+    this.confidenceLimit = 95;
+  }
+
+  /**
+   * Shorten the provided number
+   * @param {number} number the number you want to shorten
+   * @param {number} limit how far you want to limit to, default = 5
+   * @returns {number}
+   */
+  #shorten(number, limit = 5) {
+    if (typeof number === "number") {
+      return number.toFixed(limit);
+    }
+  }
+
+  averageValue() {
+    if (this.nums.length === 0) return 0;
+    const sum = this.nums.reduce((prev, curr) => prev + curr, 0);
+    return this.#shorten(sum / this.nums.length, 3);
+  }
+
+  standardError() {
+    if (this.nums.length === 0 || this.nums.length === 1) return 0;
+    const numerator = this.nums.reduce((prev, curr) => {
+      return prev + Math.pow(curr - this.averageValue(), 2);
+    }, 0);
+    const denominator = this.nums.length * (this.nums.length - 1);
+
+    return this.#shorten(Math.sqrt(numerator / denominator));
+  }
+
+  deviceError() {
+    return this.#shorten(
+      this.type === "vernier" ? this.smallestScale : this.smallestScale / 2
+    );
+  }
+
+  randomError() {
+    return this.#shorten(this.standardError() * this.coefficientN);
+  }
+
+  measurementError() {
+    return this.#shorten((this.deviceError() / 3) * this.coefficientInf);
+  }
+
+  absoluteError() {
+    return this.#shorten(
+      Math.sqrt(
+        Math.pow(this.measurementError(), 2) + Math.pow(this.randomError(), 2)
+      )
+    );
+  }
+
+  relativeError() {
+    if (this.averageValue() === 0) return 0;
+    return this.#shorten((this.absoluteError() / this.averageValue()) * 100);
+  }
+
+  confidenceLimitInt() {
+    return this.#shorten(this.confidenceLimit * 0.01, 2);
+  }
+
+  calculate() {
+    return `<b>x</b> = ${this.averageValue()} ± ${this.absoluteError()} [unit], <b>ε</b> = ${this.relativeError()}%, at <b>β</b> = ${this.confidenceLimitInt()}
+    `;
+  }
+}
+
+const example = new DirectMeasurement(
+  [155.05, 155.2, 155.25, 154.9, 155.5],
+  0.05,
+  2.145
+);
+
+// console.log("------------ Example ------------");
+// console.log("Average Value: " + example.averageValue());
+// console.log("Standard Error: " + example.standardError());
+// console.log("Random Error: " + example.randomError());
+// console.log("Device Error: " + example.deviceError());
+// console.log("Measurement Error: " + example.measurementError());
+// console.log("Absolute Error: " + example.absoluteError());
+// console.log("Result: " + example.calculate());
--- main.js
+++ main.js
@@ -1,22 +1,15 @@
 // import packages
 import { v4 as uuidv4 } from "uuid";
+import DirectMeasurement from "./Direct";
 
 // DOM elements
 const tbodyEl = document.getElementById("tbody");
 const formEl = document.getElementById("form");
 const inputEl = formEl.querySelector("#input-value");
+const calculateBtn = document.querySelector('[data-action="calculate"]');
 
 // data
-let data = [
-  {
-    id: uuidv4(),
-    value: 0,
-  },
-  {
-    id: uuidv4(),
-    value: 0,
-  },
-];
+let data = [];
 
 // custom variables
 let isEditing = false;
@@ -30,6 +23,67 @@ window.addEventListener("DOMContentLoaded", () => {
 formEl.addEventListener("submit", (e) => {
   e.preventDefault();
   addNewValue(inputEl);
+});
+
+calculateBtn.addEventListener("click", () => {
+  const inputsEl = document.querySelectorAll("[data-input]");
+  const outputsEl = document.querySelectorAll("[data-output]");
+  let arr = [],
+    coefficient = "",
+    smallestScale = "",
+    type = "";
+
+  for (let i of inputsEl) {
+    switch (i.dataset.input) {
+      case "value":
+        arr.push(parseFloat(i.innerText));
+        break;
+      case "coefficient":
+        coefficient = parseFloat(i.value);
+        break;
+      case "sm-scale":
+        smallestScale = parseFloat(i.value);
+        break;
+      case "type":
+        type = i.value;
+        break;
+      default:
+        break;
+    }
+  }
+
+  const direct = new DirectMeasurement(arr, smallestScale, coefficient, type);
+
+  for (let i of outputsEl) {
+    switch (i.dataset.output) {
+      case "average":
+        i.innerHTML = direct.averageValue();
+        break;
+      case "standard-error":
+        i.innerHTML = direct.standardError();
+        break;
+      case "device-error":
+        i.innerHTML = direct.deviceError();
+        break;
+      case "random-error":
+        i.innerHTML = direct.randomError();
+        break;
+      case "measurement-error":
+        i.innerHTML = direct.measurementError();
+        break;
+      case "absolute-error":
+        i.innerHTML = direct.absoluteError();
+        break;
+      case "relative-error":
+        i.innerHTML = direct.relativeError();
+        break;
+      case "result":
+        i.innerHTML = direct.calculate();
+        break;
+      default:
+        break;
+    }
+  }
 });
 
 // Functions
@@ -83,7 +137,7 @@ function renderRows() {
 function performActions(e) {
   const parentRow = e.target.parentElement.parentElement;
   const parentId = parentRow.dataset.id;
-  const value = parentRow.querySelector("[data-value]");
+  const value = parentRow.querySelector('[data-input="value"]');
 
   switch (e.target.dataset.action) {
     case "delete":
